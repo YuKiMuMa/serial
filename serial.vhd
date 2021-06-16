60c85e2812da79e5199d9b3fa15637b41b7e5388
YuKiMuMa <kidosan3.14@gmail.com>
2021-06-16 17:51:15 +0900

    serial01

--- serial.vhd
+++ serial.vhd
@@ -11,29 +11,62 @@ end serial;
 
 architecture Behavioral of serial is
 
-	signal Adone : std_logic :='0';
-	signal X_count_pos : std_logic_vector(18 downto 0);
+	signal bsy : std_logic :='0';
 	signal count : std_logic_vector(18 downto 0);
+	signal r_read : std_logic;
+	signal b_count : std_logic_vector(2 downto 0);
 
 begin
+
 
 process(CLK)
 begin
 	if CLK'event and CLK='1' then
-		if DD < "10110001000000" then
-			DD <= DD +'1';
-		elsif DD = "10110001000000" then
-			DD <= "00000000000000";
+		if bsy = '1' then
+			if count < "1010001011000" then
+				count <= count + '1';
+			elsif count = "1010001011000" then
+				count <="0000000000000";
+			end if;
+		else 
+			if RX = '0' then
+				bsy <= '1';
+			else
+				bsy <= '0';
+			end if;
 		end if;
 	end if;
 end process;
 
-process(DD)
+process(count)
 begin
-	if DD="01011000100000" then
-		CLK100 <= '1';
-	elsif DD="00000000000000" then
-		CLK100 <= '0';
+	if count="1010001011000" then
+		r_read <= '1';
+	else
+		r_read <= '0';
+	end if;
+end process;
+
+process(r_read)
+begin
+	if r_read'event and r_read='1' then
+		if b_count < "111" then
+			if RX = '1' then
+				s_result <= std_logic_vector(shift_right(unsigned(s_result), 1));
+				s_result <= s_result + "10000000";
+				b_count <= b_count + '1';
+			else
+				s_result <= std_logic_vector(shift_right(unsigned(s_result), 1));
+				s_result <= s_result + "00000000";
+				b_count <= b_count + '1';
+			end if;
+		elsif b_count = "111" then
+			s_result <= "00000000";
+			b_count <= "000";
+			bsy <='0';
+		end if;
+	else
+		r_read <= '0';
 	end if;
 end process;
 

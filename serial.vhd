bd80f161e62f16f1bde0dc0331f06325e48741bb
YuKiMuMa <kidosan3.14@gmail.com>
2021-06-25 17:14:16 +0900

    serial02

--- serial.vhd
+++ serial.vhd
@@ -6,15 +6,20 @@ use ieee.numeric_std.all;
 entity serial is
 
     Port ( 	CLK : in std_logic;
-			RX : in std_logic);
+			RX : in std_logic;
+			result : out std_logic_vector(7 downto 0)
+			result1 : out std_logic_vector(7 downto 0)
+			result2 : out std_logic_vector(7 downto 0)
+			result3 : out std_logic_vector(7 downto 0));
 end serial;
 
 architecture Behavioral of serial is
 
-	signal bsy : std_logic :='0';
-	signal count : std_logic_vector(18 downto 0);
+	signal bsy : std_logic;
+	signal count : std_logic_vector(12 downto 0);
 	signal r_read : std_logic;
-	signal b_count : std_logic_vector(2 downto 0);
+	signal b_count : std_logic_vector(3 downto 0);
+	signal s_result : std_logic_vector(7 downto 0):="00000000";
 
 begin
 
@@ -23,9 +28,23 @@ process(CLK)
 begin
 	if CLK'event and CLK='1' then
 		if bsy = '1' then
-			if count < "1010001011000" then
+			if count < "0000000001010" then
 				count <= count + '1';
-			elsif count = "1010001011000" then
+			elsif count = "0000000001010" then
+				if b_count < "1000" then
+					if RX = '1' then
+						s_result <= std_logic_vector(shift_right(unsigned(s_result), 1))+ "10000000";
+						b_count <= b_count + '1';
+					else
+						s_result <= std_logic_vector(shift_right(unsigned(s_result), 1));
+						b_count <= b_count + '1';
+					end if;
+				elsif b_count = "1000" then
+					b_count <= "0000";
+					result <= s_result;
+					s_result <= "00000000";
+					bsy <='0';
+				end if;
 				count <="0000000000000";
 			end if;
 		else 
@@ -38,37 +57,7 @@ begin
 	end if;
 end process;
 
-process(count)
-begin
-	if count="1010001011000" then
-		r_read <= '1';
-	else
-		r_read <= '0';
-	end if;
-end process;
 
-process(r_read)
-begin
-	if r_read'event and r_read='1' then
-		if b_count < "111" then
-			if RX = '1' then
-				s_result <= std_logic_vector(shift_right(unsigned(s_result), 1));
-				s_result <= s_result + "10000000";
-				b_count <= b_count + '1';
-			else
-				s_result <= std_logic_vector(shift_right(unsigned(s_result), 1));
-				s_result <= s_result + "00000000";
-				b_count <= b_count + '1';
-			end if;
-		elsif b_count = "111" then
-			s_result <= "00000000";
-			b_count <= "000";
-			bsy <='0';
-		end if;
-	else
-		r_read <= '0';
-	end if;
-end process;
 
 
 
